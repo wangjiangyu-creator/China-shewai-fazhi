95dcd9044ef4600cca88d1ccdd9b6534fbbfc92a
wangjiangyu-creator <wangjiangyu@gmail.com>
2026-05-24 15:06:34 +0800

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 import { defineConfig } from "astro/config";
 
 export default defineConfig({
-  site: "https://example.org",
+  site: "https://wangjiangyu-creator.github.io",
+  base: "/China-shewai-fazhi",
   trailingSlash: "ignore"
 });
